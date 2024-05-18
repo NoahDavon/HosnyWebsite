cf58335e045d3f3ad172bc3108c9d4a7f1d128b9
Mohammed <MohammedE.Fahmy@outlook.com>
2024-05-18 19:35:51 +0300

    Hover

--- elite-media-house/app/page.tsx
+++ elite-media-house/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gradient-to-r from-sky-500 to-indigo-500">
-      <div className="z-10 w-full max-w-5xl items-center justify-center  text-6xl flex font-extrabold text-black h-[100vh] m-auto text-center">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gradient-to-r from-sky-500 to-indigo-500 h-[100vh]">
+      <div className="z-10 max-w-5xl items-center justify-center font-sans  text-3xl hover:scale-125 transition-all hover:text-white flex font-extrabold text-black m-auto text-center">
       Under Construction
       </div>
     </main>
