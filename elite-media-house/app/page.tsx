c488d157c91e8beae218a09043800e10865213d8
Mohammed <MohammedE.Fahmy@outlook.com>
2024-05-18 19:07:29 +0300

    Under construction

---
+++ elite-media-house/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gradient-to-r from-sky-500 to-indigo-500">
+      <div className="z-10 w-full max-w-5xl items-center justify-center  text-6xl flex font-extrabold text-black h-[100vh] m-auto text-center">
+      Under Construction
+      </div>
+    </main>
+  );
+}
