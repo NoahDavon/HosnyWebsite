4709ca06b1ef83dd8e3c0f991ef02f47ef521556
Mohammed <MohammedE.Fahmy@outlook.com>
2024-05-21 20:55:07 +0300

    Under construction images

--- elite-media-house/app/layout.tsx
+++ elite-media-house/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({ weight: "700", subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Elite Media Houses",
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={"bg-[#00B3D1] " + roboto.className}>{children}</body>
     </html>
   );
 }
--- elite-media-house/app/page.tsx
+++ elite-media-house/app/page.tsx
@@ -1,10 +1,74 @@
 import Image from "next/image";
+import ConstructionIcon from "./ConstructionIcon";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gradient-to-r from-sky-500 to-indigo-500 h-[100vh]">
-      <div className="z-10 max-w-5xl items-center justify-center font-sans  text-3xl hover:scale-125 transition-all hover:text-white flex font-extrabold text-black m-auto text-center">
-      Under Construction
+    <main className="w-full h-screen p-24 bg-[#00B3D1]">
+      <Image
+        alt="logo"
+        width={147}
+        height={147}
+        src={"/EMH logo 2.png"}
+        className="mx-auto lg:mx-0"
+      />
+      <div className="flex lg:justify-between justify-start lg:items-center items-start h-full flex-wrap-reverse lg:mx-0 mx-auto">
+        <div className="flex flex-col items-start gap-2 lg:w-2/3 w-full pb-2 lg:pb-0 mx-auto lg:mx-0">
+          <div className="lg:text-7xl font-serif font-bold text-2xl lg:text-left text-center ">
+            Our New Advertising Services Website Is Coming Soon
+          </div>
+          <div className="lg:text-4xl font-semibold text-base lg:text-left text-center m-auto lg:m-0">
+            Made By Elites For Elites
+          </div>
+        </div>
+        <ConstructionIcon />
+      </div>
+      <div className="flex flex-col items-center w-full gap-3 pb-8">
+        <div className="lg:text-[32px] font-medium text-base text-center">
+          Work with us
+        </div>
+        <div className="lg:flex hidden gap-6">
+          <Link
+            href={
+              "https://www.instagram.com/elite_media_house?igsh=M2ZtdGEyOW0yMzg3"
+            }
+          >
+            <Image
+              width={70}
+              alt="insta"
+              height={70}
+              src={"/instagram_4494488 1.png"}
+            />
+          </Link>
+          <Link
+            href={
+              "https://www.linkedin.com/company/elite-media-house/about/?viewAsMember=true"
+            }
+          >
+            <Image width={70} alt="insta" height={70} src={"/145807 1.png"} />
+          </Link>
+        </div>
+        <div className="flex lg:hidden gap-6">
+          <Link
+            href={
+              "https://www.instagram.com/elite_media_house?igsh=M2ZtdGEyOW0yMzg3"
+            }
+          >
+            <Image
+              width={55}
+              alt="insta"
+              height={55}
+              src={"/instagram_4494488 1.png"}
+            />
+          </Link>
+          <Link
+            href={
+              "https://www.linkedin.com/company/elite-media-house/about/?viewAsMember=true"
+            }
+          >
+            <Image width={55} alt="insta" height={55} src={"/145807 1.png"} />
+          </Link>
+        </div>
       </div>
     </main>
   );
